ec4bddb8f2e13ce3b0ab97b39d2ef1e65241c982
Alexander Magee <86931568+xmagee@users.noreply.github.com>
2024-05-13 19:21:48 -0400

    basic form for company input, validation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,104 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Company } from './models/company'
 import './app.css'
 
 export default function App() {
-  let [newCompany, updateCompany] = 
-    useState(new Company('','','','','','','','','','','','',true))
+  const [companyFormError, setCompanyFormError] = useState(''),
+   [newCompany, setNewCompany] = useState(new Company('','','','','','','','','','','','')), 
+   reqFieldMessage = 'Missing required field: ';
 
-  function updateCompanyProperty(propertyName, newValue) { 
+  function setCompanyInputInvalid(elementId) { 
+    const thisInput = document.getElementById(`company_${elementId}_input`);
+
+    if (thisInput) { 
+      thisInput.focus();
+      thisInput.classList.add('is-danger');
+    }
+  }
+
+  function clearInvalidInputStyle() { 
+    setCompanyFormError('');
+
+    const formInputs = document.getElementsByClassName('company-input');
+
+    for (let i = 0; i < formInputs.length; i++) { 
+      formInputs[i].classList.remove('is-danger');
+    }
+  }
+
+  function updateCompanyProperty(event) { 
     let c = newCompany;
-    c[propertyName] = newValue;
-    updateCompany(c);
+    c[event.target.id.split('_')[1]] = event.target.value;
+    setNewCompany(c);
   }
 
-  function validateCompanyObj() { 
-    // TODO
+  async function validateCompanyObj() { 
+    return new Promise(resolve => {
+      if (newCompany.title === '') { 
+        setCompanyInputInvalid('title');
+        resolve(`${reqFieldMessage} title`);
+        setCompanyFormError(`${reqFieldMessage} title`)
+        return;
+      }
+
+      if (newCompany.hqLocation === '') { 
+        setCompanyInputInvalid('hqLocation');
+        resolve(`${reqFieldMessage} hqLocation`);
+        setCompanyFormError(`${reqFieldMessage} hqLocation`)
+        return;
+      }
+
+      if (newCompany.companyContent === '') { 
+        setCompanyInputInvalid('companyContent');
+        resolve(`${reqFieldMessage} companyContent`);
+        setCompanyFormError(`${reqFieldMessage} companyContent`)
+        return;
+      }
+
+      resolve(null)
+      return;
+    })
   }
 
-  function submitCompany() {
-    // TODO: check obj validations
-    // TODO: firestore insert 
+  async function submitCompany() {
+    console.info('no error!', newCompany)
+    clearInvalidInputStyle();
+
+    const error = await validateCompanyObj();
+
+    if (!error) { 
+      console.info('no error!', newCompany)
+      // TODO create rec in firestore ... 
+    } else { 
+      console.error(error)
+    }
   }
 
   return (
-    <>
-        <div>
-          <p>insert new company:</p>
-          {Object.keys(newCompany).map((p, i) => (
-            <div key={i}>
-              { `${p}: ` }
-              <input value={newCompany[p]} onChange={(e) => updateCompanyProperty(p, e.target.value)} />
+   <div className="box">
+      <div className="grid has-2-cols">
+        <div className="card cell box">
+            <h1 className="title">Insert New Company</h1>
+            
+            {Object.keys(newCompany).map((p, i) => (
+                <div className="field" key={i}>
+                  <label className="label">{p}</label>
+                  <input id={`company_${p}_input`} defaultValue={newCompany[p]} 
+                    className="input company-input" placeholder={p} onChange={updateCompanyProperty} />
+                </div>
+            ))}
+       
+            <div>
+              <button type="submit" className="button is-primary is-pulled-right" onClick={submitCompany}>INSERT</button>
+              <button className="button is-danger is-pulled-right">CLEAR</button>
+              {companyFormError && <p className="has-text-danger">{companyFormError}</p>}
             </div>
-          ))}
-          <button type="submit" onClick={submitCompany}>INSERT</button>
         </div>
-    </>
+
+        <div className="card cell">
+          ... Insert New Job TODO ...
+        </div>
+      </div>
+    </div>
   );
 };
