9e797a21638b72707aad56a039a13b886c9979ab
Alexander Magee <86931568+xmagee@users.noreply.github.com>
2024-07-03 17:30:37 -0400

    remove uneeded model

--- src/models/prt.js
+++
@@ -1,8 +0,0 @@
-// page, route, title
-export default class PRT { 
-    constructor(page, route, title) { 
-        this.page = page;
-        this.route = route;
-        this.title = title;
-    }
-}
